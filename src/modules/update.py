13fa5fcefabf14828ce97c264076177a506ec1d0
MaxLastBreath <yusei.tqnev@gmail.com>
2023-07-31 14:22:02 +0300

    Starting work for 1.2.0

--- src/modules/update.py
+++ src/modules/update.py
@@ -8,7 +8,7 @@ import subprocess
 import tkinter as tk
 from tkinter import messagebox
 
-Version = "manager-1.1.2"
+Version = "manager-1.2.0"
 textver = Version.strip("manager-")
 GITHUB = "TOTK-mods"
 OWNER = "MaxLastBreath"
