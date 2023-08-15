92924069664e05290d692ff462203d3e6d4c5c2d
MaxLastBreath <yusei.tqnev@gmail.com>
2023-08-15 20:23:19 +0300

    h

--- src/modules/scaling.py
+++ src/modules/scaling.py
@@ -4,20 +4,19 @@ import platform
 from screeninfo import get_monitors
 
 def Auto_SF():
-    if platform.system() == "Windows":
-        monitors = get_monitors()
-        current_monitor = monitors[0]
-        h = current_monitor.height
-        print(h)
-        sf = 1.0
+    monitors = get_monitors()
+    current_monitor = monitors[0]
+    h = current_monitor.height
+    print(h)
+    sf = 1.0
 
-        if h >= 1080:
-            sf = 1.0
-        if h >= 1440:
-            sf = 1.5
-        if h >= 2160:
-            sf = 2.0
-        return sf
+    if h >= 1080:
+        sf = 1.0
+    if h >= 1440:
+        sf = 1.5
+    if h >= 2160:
+        sf = 2.0
+    return sf
 
 # Use First Monitor to determine SF, this bypasses scaling from windows.
 sf = Auto_SF()
--- src/modules/update.py
+++ src/modules/update.py
@@ -9,7 +9,7 @@ import subprocess
 import tkinter as tk
 from tkinter import messagebox
 
-Version = "manager-1.2.1"
+Version = "manager-1.2.3"
 textver = Version.strip("manager-")
 GITHUB = "TOTK-mods"
 OWNER = "MaxLastBreath"
