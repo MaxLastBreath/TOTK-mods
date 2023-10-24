b6461b69257b3af666b1e6af94bcc5629d50d3c0
MaxLastBreath <yusei.tqnev@gmail.com>
2023-10-24 15:20:58 +0300

    Fix utf8 cant detect if application is running

--- src/modules/launch.py
+++ src/modules/launch.py
@@ -4,12 +4,15 @@ from tkinter import filedialog, Toplevel
 from configuration.settings import *
 
 def is_process_running(process_name):
-    cmd = 'tasklist /fi "imagename eq {}"'.format(process_name)
-    output = subprocess.check_output(cmd, shell=True).decode()
-    if process_name.lower() in output.lower():
-        return True
-    else:
-        return False
+    try:
+        cmd = 'tasklist /fi "imagename eq {}"'.format(process_name)
+        output = subprocess.check_output(cmd, shell=True).decode()
+        if process_name.lower() in output.lower():
+            return True
+        else:
+            return False
+    except Exception as e:
+        log.info(f"Couldn`t detect if {process_name} is running.}")
 
 def select_game_file(command=None):
     # Open a file dialog to browse and select yuzu.exe
