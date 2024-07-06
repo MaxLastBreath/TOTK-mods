cec819ab6aced984b687159f6438d376df6aa4e1
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-07-06 23:25:21 +0700

    debug macos intel version

--- src/compile_onedir.py
+++ src/compile_onedir.py
@@ -66,6 +66,7 @@ if __name__ == "__main__":
             "--target-arch=x86_64",
             "--onedir",
             "--windowed",
+            "--debug=all",
             "--noconfirm",
             f"--name=TOTK Optimizer",
             "run.py",
