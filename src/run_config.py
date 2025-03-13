495e542e1abe1d55510b8c9c1ffe03230ae834c7
MaxLastBreath <yusei.tqnev@gmail.com>
2025-03-13 17:06:51 +0200

    Update sys.MEIPASS

--- src/run_config.py
+++ src/run_config.py
@@ -2,5 +2,5 @@ import sys, os
 
 __ROOT__ = os.path.dirname(__file__)
 
-# if getattr(sys, "frozen", False):
-#     __ROOT__ = sys._MEIPASS
+if getattr(sys, "frozen", False):
+    __ROOT__ = sys._MEIPASS
